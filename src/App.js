83db6a037e97fd13eac3ea8f4fdbe0bdaef1e09d
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-01 00:14:33 +0200

    adds code refactor to match task 3 provisions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import Container from "./Components/Container";
-import Feedback from "./Components/Feedback";
+import Section from "./Components/Section";
+import Controls from "./Components/Controls";
+import Statistics from "./Components/Statistics";
+import Notification from "./Components/Notification";
 
-function App() {
-  return (
-    <Container>
-      <Feedback />
-    </Container>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return +((this.state.good / this.countTotalFeedback()) * 100).toFixed(0);
+  };
+
+  handleFeedback = (e) => {
+    const feedbackType = e.target.dataset.action;
+    this.setState((prevState) => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+    e.target.blur();
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <Controls onClick={this.handleFeedback} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              percent={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </Container>
+    );
+  }
 }
 
 export default App;
